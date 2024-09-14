521c6bcf47a74478bd21281066f95bd61dc89ca2
Ronald Santos <ronalddesign10@gmail.com>
2024-09-14 11:13:20 -0300

    layout base

---
+++ src/app/(ui)/layout.tsx
@@ -0,0 +1,37 @@
+import { NavItem } from "@/components/nav/nav-item";
+import { NavLogout } from "@/components/nav/nav-logout";
+import { Logo } from "@/components/ui/logo";
+import { faHouse, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
+      <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-zinc-900">
+        <div className="flex-1 flex flex-col gap-11 mt-6">
+          <Logo size={24} />
+          <nav>
+            <NavItem
+              label="Página inicial"
+              icon={faHouse}
+              href="/home"
+              active
+            />
+            <NavItem label="Meu Perfil" icon={faUserAlt} href="/profile" />
+          </nav>
+        </div>
+        <div className="mb-6 flex flex-col gap-4">
+            <NavLogout/>
+        </div>
+      </section>
+      <section className="flex-1 max-w-lg">{children}</section>
+      <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-zinc-900">
+        ...
+      </aside>
+    </main>
+  );
+}
---
+++ src/components/nav/nav-item.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import type { IconDefinition } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  label: string;
+  icon: IconDefinition;
+  href: string;
+  active?: boolean;
+};
+export const NavItem = ({ label, icon, href, active }: Props) => {
+  const pathName = usePathname();
+  const isMe = pathName === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center gap-6 py-3 ${
+        active || isMe ? "opacity-100" : "opacity-70"
+      } hover:opacity-100`}
+    >
+      <FontAwesomeIcon icon={icon} className="size-5" />
+      <div className="text-lg">{label}</div>
+    </Link>
+  );
+};
